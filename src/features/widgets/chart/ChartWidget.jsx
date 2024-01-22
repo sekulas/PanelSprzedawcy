e765ac8b1ee99e5b94761088f540ef082bf8488c
Dawid Cybulski <100097374+davidcybulsky@users.noreply.github.com>
2024-01-22 12:32:23 +0100

    Reactive Options

--- src/features/widgets/chart/ChartWidget.jsx
+++ src/features/widgets/chart/ChartWidget.jsx
@@ -1,10 +1,41 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import LanguageContext from '../../languages/LanguageContext';
 import WidgetTypeBox from "../WidgetTypeBox";
 
 const ChartWidget = () => {
 
     const { dictionary } = useContext(LanguageContext);
+    const [obrot, setObrot] = useState(true);
+    const [slupkowy, setSlupkowy] = useState(true);
+    const [period, setPeriod] = useState(0);
+
+    const setObrotTrue = () => {
+        setObrot(true)
+    }
+
+    const setObrotFalse = () => {
+        setObrot(false)
+    }
+
+    const setSlupkowyTrue = () => {
+        setSlupkowy(true)
+    }
+
+    const setSlupkowyFalse = () => {
+        setSlupkowy(false)
+    }
+
+    const setPeriod0 = () => {
+        setPeriod(0)
+    }
+
+    const setPeriod1 = () => {
+        setPeriod(1)
+    }
+    
+    const setPeriod2 = () => {
+        setPeriod(2)
+    }
 
     return (
     <div className="widget">
@@ -14,19 +45,19 @@ const ChartWidget = () => {
         <div className="chart-widget_content_menu">
             <div className="chart-widget_content_choice">
                 <h3>{dictionary.sellersPanel.salesChart.measurement}</h3>
-                <button className="chart-widget_content_choice_option_2 chart-widget_content_choice_option_clicked">Obrót</button>
-                <button className="chart-widget_content_choice_option_2">Sprzedanych</button>
+                <button className={`chart-widget_content_choice_option_2 ${obrot ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setObrotTrue}>Obrót</button>
+                <button className={`chart-widget_content_choice_option_2 ${!obrot ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setObrotFalse}>Sprzedanych</button>
             </div>
             <div className="chart-widget_content_choice">
                 <h3>{dictionary.sellersPanel.salesChart.chart}</h3>
-                <button className="chart-widget_content_choice_option_2 chart-widget_content_choice_option_clicked">Słupkowy</button>
-                <button className="chart-widget_content_choice_option_2">Liniowy</button>
+                <button className={`chart-widget_content_choice_option_2 ${slupkowy ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setSlupkowyTrue}>Słupkowy</button>
+                <button className={`chart-widget_content_choice_option_2 ${!slupkowy ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setSlupkowyFalse}>Liniowy</button>
             </div>
             <div className="chart-widget_content_choice">
                 <h3>{dictionary.sellersPanel.salesChart.period}</h3>
-                <button className="chart-widget_content_choice_option_3 chart-widget_content_choice_option_clicked">Dziś</button>
-                <button className="chart-widget_content_choice_option_3">Obecny tydzień</button>
-                <button className="chart-widget_content_choice_option_3">Poprzedni tydzień</button>
+                <button className={`chart-widget_content_choice_option_3 ${period==0 ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setPeriod0}>Dziś</button>
+                <button className={`chart-widget_content_choice_option_3 ${period==1 ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setPeriod1}>Obecny tydzień</button>
+                <button className={`chart-widget_content_choice_option_3 ${period==2 ? 'chart-widget_content_choice_option_clicked' : ""}`} onClick={setPeriod2}>Poprzedni tydzień</button>
             </div>
         </div>
         <div className="chart-widget_content_chart">
--- src/features/widgets/opinions/OpinionsWidget.jsx
+++ src/features/widgets/opinions/OpinionsWidget.jsx
@@ -1,11 +1,24 @@
 import Opinion  from "./Opinion"
 import { useNavigate } from "react-router-dom";
 import WidgetTypeBox from "../WidgetTypeBox";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import LanguageContext from '../../languages/LanguageContext';
 
 const OpinionWidget = () => {
     const { dictionary } = useContext(LanguageContext);
+    const [period, setPeriod] = useState(0);
+
+    const setPeriod0 = () => {
+        setPeriod(0)
+    }
+
+    const setPeriod1 = () => {
+        setPeriod(1)
+    }
+    
+    const setPeriod2 = () => {
+        setPeriod(2)
+    }
 
     const navigate = useNavigate();
 
@@ -17,9 +30,9 @@ const OpinionWidget = () => {
     <div className="widget" >
         <WidgetTypeBox widgetType={dictionary.sellersPanel.customersOpinions.title} onNavigate={onNavigateClicked}/>      
         <div className="opinion-widget_content_choice">
-                <button className="opinion-widget_content_choice_option_3 opinion-widget_content_choice_option_clicked">Dziś</button>
-                <button className="opinion-widget_content_choice_option_3">Obecny tydzień</button>
-                <button className="opinion-widget_content_choice_option_3">Poprzedni tydzień</button>
+                <button className={`opinion-widget_content_choice_option_3 ${period == 0 ? 'opinion-widget_content_choice_option_clicked' : ''}`} onClick={setPeriod0}>Dziś</button>
+                <button className={`opinion-widget_content_choice_option_3 ${period == 1 ? 'opinion-widget_content_choice_option_clicked' : ''}`} onClick={setPeriod1}>Obecny tydzień</button>
+                <button className={`opinion-widget_content_choice_option_3 ${period == 2 ? 'opinion-widget_content_choice_option_clicked' : ''}`} onClick={setPeriod2}>Poprzedni tydzień</button>
             </div>
         <div className="opinion-widget_content">
             <Opinion></Opinion>
