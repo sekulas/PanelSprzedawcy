2e85b59fae41e2e24e05159fbb7d44f4d7b1ad5a
Dawid Cybulski <100097374+davidcybulsky@users.noreply.github.com>
2024-01-22 21:12:15 +0100

    Customers opinions

--- src/features/widgets/opinions/Opinions.jsx
+++ src/features/widgets/opinions/Opinions.jsx
@@ -1,5 +1,5 @@
 import Opinion  from "./Opinion";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import LanguageContext from '../../languages/LanguageContext';
 import ShopContext from "../../multiple-shop-accounts/ShopContext";
 
@@ -7,6 +7,17 @@ const Opinions = () => {
 const { dictionary } = useContext(LanguageContext);
 const [period, setPeriod] = useState(0);
 const { shopData } = useContext(ShopContext);
+const [opinions, setOpinions] = useState(shopData.opinions.today);
+
+useEffect(() => {
+    if(period == 0){
+        setOpinions(shopData.opinions.today)
+    } else if(period == 1){
+        setOpinions(shopData.opinions.thisWeek)
+    } else if(period == 2) {
+        setOpinions(shopData.opinions.prevWeek)
+    }
+}, [shopData, period])
 
 const setPeriod0 = () => {
     setPeriod(0)
@@ -27,9 +38,9 @@ return(<div>
         <button className={`opinion-widget_content_choice_option_3 ${period == 2 ? 'opinion-widget_content_choice_option_clicked' : ''}`} onClick={setPeriod2}>{dictionary.sellersPanel.customersOpinions.previousWeek}</button>
     </div>
     <div className="opinion-widget_content">
-        <Opinion option={shopData.opinions[0]}></Opinion>
-        <Opinion option={shopData.opinions[1]}></Opinion>    
-        <Opinion option={shopData.opinions[2]}></Opinion>
+        <Opinion option={opinions[0]}></Opinion>
+        <Opinion option={opinions[1]}></Opinion>    
+        <Opinion option={opinions[2]}></Opinion>
     </div>
 </div>);
 }
